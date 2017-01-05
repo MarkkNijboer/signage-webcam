4a78b24bbac92e909eb39ee5012ab4f61a809c2c
Mark Nijboer <mark@Marks-MacBook-Pro.local>
2017-01-05 20:29:33 +0100

    Initial Commit

---
+++ cameraTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cameraTest
+//
+//  Created by Mark Nijboer on 05/01/2017.
+//  Copyright © 2017 Mark Nijboer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
